a408f7d817729840b0dd328fd4b1d81f93a81adc
akarachevtsev <akarachevtsev@amtoss.com>
2021-06-30 14:36:36 +0300

    Warriors are preparing for battle.

--- BigGame/BigGame_Console/Program.cs
+++ BigGame/BigGame_Console/Program.cs
@@ -2,11 +2,56 @@
 
 namespace BigGame_Console
 {
+    enum Warrior
+    {
+        Spearman = 'К',
+        Bowman = 'Л'
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Warrior[] orks = new Warrior[8] 
+            { 
+                Warrior.Spearman, 
+                Warrior.Spearman, 
+                Warrior.Spearman, 
+                Warrior.Spearman, 
+                Warrior.Spearman, 
+                Warrior.Bowman, 
+                Warrior.Bowman, 
+                Warrior.Bowman 
+            };
+
+            Warrior[] elfs = new Warrior[10]
+            {
+                Warrior.Bowman,
+                Warrior.Bowman,
+                Warrior.Bowman,
+                Warrior.Bowman,
+                Warrior.Bowman,
+                Warrior.Bowman,
+                Warrior.Spearman,
+                Warrior.Spearman,
+                Warrior.Spearman,
+                Warrior.Spearman
+            };
+
+            Console.Write("Орки:\t");
+            for (int i = 0; i < orks.Length; i++)
+            {
+                Console.Write($" {(char)orks[i]}");
+            }
+            
+            Console.Write("\nЭльфы:\t");
+            for (int i = 0; i < elfs.Length; i++)
+            {
+                Console.Write($" {(char)elfs[i]}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
