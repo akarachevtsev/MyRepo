776cb8dea7aeb715113d0ca507cb6e627a8f17b5
akarachevtsev <akarachevtsev@amtoss.com>
2021-07-08 11:52:01 +0300

    Battle begins.

--- BigGame/BigGame_Console/Program.cs
+++ BigGame/BigGame_Console/Program.cs
@@ -12,60 +12,85 @@ namespace BigGame_Console
     {
         static void Main(string[] args)
         {
-
             int spearmanDamageValue = 3;
             int bowmanDamageValue = 5;
             int spearmanHpValue = 11;
             int bowmanHpValue = 7;
 
-            var orks = new (Warrior warriorType, int healthPoints)[]
+            var orks = new (Warrior warriorType, int healthPoints, int damageValue)[]
             {
-                (Warrior.Spearman, spearmanHpValue),
-                (Warrior.Spearman, spearmanHpValue),
-                (Warrior.Spearman, spearmanHpValue),
-                (Warrior.Spearman, spearmanHpValue),
-                (Warrior.Spearman, spearmanHpValue),
-                (Warrior.Bowman, bowmanHpValue),
-                (Warrior.Bowman, bowmanHpValue),
-                (Warrior.Bowman, bowmanHpValue)
+                (Warrior.Spearman, spearmanHpValue, spearmanDamageValue),
+                (Warrior.Spearman, spearmanHpValue, spearmanDamageValue),
+                (Warrior.Spearman, spearmanHpValue, spearmanDamageValue),
+                (Warrior.Spearman, spearmanHpValue, spearmanDamageValue),
+                (Warrior.Spearman, spearmanHpValue, spearmanDamageValue),
+                (Warrior.Bowman, bowmanHpValue, bowmanDamageValue),
+                (Warrior.Bowman, bowmanHpValue, bowmanDamageValue),
+                (Warrior.Bowman, bowmanHpValue, bowmanDamageValue)
             };
 
-            var elfs = new (Warrior warriorType, int healthPoints)[]
+            var elfs = new (Warrior warriorType, int healthPoints, int damageValue)[]
             {
-                (Warrior.Bowman, bowmanHpValue),
-                (Warrior.Bowman, bowmanHpValue),
-                (Warrior.Bowman, bowmanHpValue),
-                (Warrior.Bowman, bowmanHpValue),
-                (Warrior.Bowman, bowmanHpValue),
-                (Warrior.Bowman, bowmanHpValue),
-                (Warrior.Spearman, spearmanHpValue),
-                (Warrior.Spearman, spearmanHpValue),
-                (Warrior.Spearman, spearmanHpValue),
-                (Warrior.Spearman, spearmanHpValue)
+                (Warrior.Bowman, bowmanHpValue, bowmanDamageValue),
+                (Warrior.Bowman, bowmanHpValue, bowmanDamageValue),
+                (Warrior.Bowman, bowmanHpValue, bowmanDamageValue),
+                (Warrior.Bowman, bowmanHpValue, bowmanDamageValue),
+                (Warrior.Bowman, bowmanHpValue, bowmanDamageValue),
+                (Warrior.Bowman, bowmanHpValue, bowmanDamageValue),
+                (Warrior.Spearman, spearmanHpValue, spearmanDamageValue),
+                (Warrior.Spearman, spearmanHpValue, spearmanDamageValue)
+                //(Warrior.Spearman, spearmanHpValue, spearmanDamageValue),
+                //(Warrior.Spearman, spearmanHpValue, spearmanDamageValue)
             };
 
-            //Console.WriteLine(orks[0]); //(Spearman, 11)
-            //Console.WriteLine(orks[0].healthPoints); //11
-            //orks[0].healthPoints = orks[0].healthPoints - spearmanDamageValue;
-            //Console.WriteLine(orks[0].healthPoints); //8
+            ShowArmy("Orks:\t", orks);
+            ShowArmy("Elfs:\t", elfs);
 
-            Console.Write("Орки:\t");
-            foreach (var i in orks)
-                ShowWarrior(i.warriorType, i.healthPoints);
-
-            Console.Write("\nЭльфы:\t");
-            foreach (var i in elfs)
-                ShowWarrior(i.warriorType, i.healthPoints);
-
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Orks attack!");
+                for (int i = 0; i < orks.Length; i++)
+                    Attack(orks[i].healthPoints, ref elfs[i].healthPoints, orks[i].damageValue);
+                ShowArmy("Orks:\t", orks);
+                ShowArmy("Elfs:\t", elfs);
+                if (ArmyTotalHealth(elfs) == 0)
+                    break;
+                Console.WriteLine();
+                Console.WriteLine("Elfs attack!");
+                for (int i = 0; i < orks.Length; i++)
+                    Attack(elfs[i].healthPoints, ref orks[i].healthPoints, elfs[i].damageValue);
+                ShowArmy("Orks:\t", orks);
+                ShowArmy("Elfs:\t", elfs);
+                if (ArmyTotalHealth(orks) == 0)
+                    break;
+            }
             Console.ReadKey();
         }
 
+        static void Attack(int attackingWarriorHealth,ref int attackedWarriorHealth, int attackingWarriorDamage)
+        {
+            if (attackingWarriorHealth > 0)
+                attackedWarriorHealth = Math.Max(attackedWarriorHealth - attackingWarriorDamage, 0);
+        }
+        static int ArmyTotalHealth(params (Warrior warriorType, int healthPoints, int damageValue)[] race)
+        {
+            int totalHealth = 0;
+            foreach (var i in race)
+                totalHealth += i.healthPoints;
+            return totalHealth;
+        }
+        static void ShowArmy(string raceName, params (Warrior warriorType, int healthPoints, int damageValue)[] race)
+        {
+            Console.Write(raceName);
+            foreach (var i in race)
+                ShowWarrior(i.warriorType, i.healthPoints);
+            Console.WriteLine();
+        }
         static void ShowWarrior(Warrior warrior, int health)
         {
-            if (warrior == Warrior.Spearman)
-                Console.Write(String.Format("{0,3}{1,3}", "К(", $"{health})"));
-            else
-                Console.Write(String.Format("{0,3}{1,3}", "Л(", $"{health})"));
+            string warriorChar = warrior == Warrior.Spearman ? "К" : "Л";
+            Console.Write($" {warriorChar}({health,2})");
         }
     }
 }
